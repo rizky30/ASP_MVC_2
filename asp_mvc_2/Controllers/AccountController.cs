a58eee6cdf7af1d2d75fc3850480e8ef9065bf4a
Muhammad Rizky Ajie Soelstyo <muhammad_soelistyo_24rpl@student.smktelkom-mlg.sch.id>
2017-02-07 08:14:46 +0700

    asp_mvc_2 Modul 2

--- asp_mvc_2/Controllers/AccountController.cs
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -15,5 +15,36 @@ namespace asp_mvc_2.Controllers
         public ActionResult SignUp() { return View(); }
         [HttpPost]
         public ActionResult SignUp(UserSignUpView USV) { if (ModelState.IsValid) { UserManager UM = new UserManager(); if (!UM.IsLoginNameExist(USV.LoginName)) { UM.AddUserAccount(USV); FormsAuthentication.SetAuthCookie(USV.FirstName, false); return RedirectToAction("Welcome", "Home"); } else ModelState.AddModelError("", "Login Name already taken."); } return View(); }
-    }
+
+        public ActionResult LogIn() {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult SignOut() {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(UserLoginView ULV, string returnUrl)
+        {
+            if (ModelState.IsValid) {
+                UserManager UM = new UserManager();
+                string password = UM.GetUserPassword(ULV.LoginName);
+                if (string.IsNullOrEmpty(password))
+                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
+                else {
+                    if (ULV.Password.Equals(password)) {
+                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
+                        return RedirectToAction("Welcome", "Home"); }
+                    else { ModelState.AddModelError("", "The password provided is incorrect.");
+                    }
+                }
+            } 
+            // If we got this far, something failed, redisplay form 
+            return View(ULV); }
+
+        }
+
 }
--- asp_mvc_2/Models/ViewModel/UserModel.cs
+++ asp_mvc_2/Models/ViewModel/UserModel.cs
@@ -28,4 +28,15 @@ namespace asp_mvc_2.Models.ViewModel
         public string Gender { get; set; }
     }
 
+    public class UserLoginView {
+        [Key] public int SYSUserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
 }
